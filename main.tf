14f296ac69214cd02d9f11625be6667834e6356b
shogomuranushi <xwkns157@gmail.com>
2021-11-07 12:02:54 +0900

    1

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
+resource "random_pet" "sg" {}
